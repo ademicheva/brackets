c8c732e2ddbc25848b4f0577e8fbdd66534c7112
Алина <alina@MacBook-Pro-Alina.local>
2020-10-19 23:35:08 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = [];
+    let bigStr = [];
+    for (i = 0; i < bracketsConfig.length; i++) {
+        for (j = 0; j < bracketsConfig[i].length; j++) {
+            arr += bracketsConfig[i][j];
+        }
+        bigStr.push(arr);
+        arr = '';
+    }
+
+    for (let i = 0; i < bigStr.length; i++) {
+        if (str.includes(bigStr[i])) {
+            str = str.replace(bigStr[i], "");
+            i = -1;
+        }
+    }
+
+    if (str.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
